c79bfc180ff494af37dd9327c6b70795a3a0d6f7
Jeriko <jcarrico1985@live.com>
2017-06-30 14:33:37 -0500

    Initial git commit, added gitignore file.

---
+++ src/main/java/com/simplesauce/LifemapApplication.java
@@ -0,0 +1,12 @@
+package com.simplesauce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifemapApplication.class, args);
+	}
+}
