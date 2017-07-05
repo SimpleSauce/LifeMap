306088a3c288eec8fcb844825902222b34fa5248
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-07-05 18:55:19 -0500

    Added the following:
    1.  UserController
    2.  Register link works properly and adds record to users table
    3.  Login view displays properly.  working on loggin in issue.
    4.  Added roles and security configuration
    5.  Added submit button to login view

---
+++ src/main/java/com/simplesauce/services/SecurityConfiguration.java
@@ -0,0 +1,76 @@
+package com.simplesauce.services;
+
+/**
+ * Created by everardosifuentes on 6/26/17.
+ */
+import com.simplesauce.models.UserWithRoles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by fer on 6/23/17.
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = UserWithRoles.class)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsLoader userDetails;
+
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/index") // user's home page, it can be any URL
+                .permitAll() // Anyone can go to the login page
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/logout") // anyone can see the home and logout page
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout") // append a query string value
+                .and()
+                .authorizeRequests()
+                .antMatchers("/posts/create", "/posts/?/edit") // only authenticated users can create ads
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetails).passwordEncoder(passwordEncoder());
+    }
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/ads/create") // only admin users can create ads
+//                .hasAuthority("ADMIN");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/ads/create") // either admin or sellers can create ads
+//                .hasAnyAuthority("ADMIN", "SELLER");
+//    }
+
+}
